57c419637f1a7bb49de6c250b05c3fffdb648a66
zhangyan <zhyabs@gmail.com>
2022-06-21 18:47:32 +0800

    commit

--- index.ts
+++ index.ts
@@ -2,24 +2,28 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 import sizeOf from "buffer-image-size";
 
-const whatElementScript = fs.readFileSync("./node_modules/whats-element/dist/whatsElement.js", "utf8");
-const fileName = "example";
+const whatElementScript = fs.readFileSync(
+  "./whatsElement.js",
+  "utf8"
+);
+
+const url = "https://fund.eastmoney.com/";
+// const url = "https://www.qcc.com/area/hun_430900";
+// const url = "http://www.gov.cn/hudong/2020-07/09/content_5525332.htm";
+const fileName = url.replace(/[/.:]/g, "");
 
 const main = async () => {
   try {
     fs.unlinkSync(`${fileName}.json`);
-  } catch(e) {}
+  } catch (e) {}
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     // slowMo: 1000,
     args: ["--window-position=0,0"],
     devtools: true,
   });
 
-  // const url = "https://fund.eastmoney.com/";
-  const url = "https://bj.58.com/xinfang/";
-  // const url = "http://www.gov.cn/hudong/2020-07/09/content_5525332.htm";
   const page = await browser.newPage();
   await page.goto(url);
   await page.waitForTimeout(10000);
@@ -43,7 +47,8 @@ const main = async () => {
   console.log("html boundingBox", { html_width, html_height });
 
   await page.setViewport({ width: html_width, height: html_height });
-  await page.waitForTimeout(10000);
+  await page.reload();
+  await page.waitForTimeout(1000);
   await page.screenshot({
     path: `${fileName}.png`,
     fullPage: true,
@@ -60,16 +65,30 @@ const main = async () => {
       return createPage(browser, html_width, html_height, url, gap * idx, gap * idx + increment)
     });
   })));
-  // const result = await createPage(browser, html_width, html_height, url, 9000, 11782);
+  // const result = await createPage(
+  //   browser,
+  //   html_width,
+  //   html_height,
+  //   url,
+  //   4500,
+  //   4700
+  // );
   // const results = [result];
-  
+
   const result_json = JSON.stringify(results.flat());
   fs.writeFileSync(`${fileName}.json`, result_json);
 
   await browser.close();
-}
+};
 
-const createPage = async (browser: puppeteer.Browser, w: number, h: number, url: string, start: number, end: number) => {
+const createPage = async (
+  browser: puppeteer.Browser,
+  w: number,
+  h: number,
+  url: string,
+  start: number,
+  end: number
+) => {
   const page = await browser.newPage();
   await page.goto(url);
   await page.waitForTimeout(10000);
@@ -90,7 +109,7 @@ const createPage = async (browser: puppeteer.Browser, w: number, h: number, url:
     const removeElement = (element: Element) => {
       const style = getComputedStyle(element);
 
-      if (style.display === "none") {
+      if (style.display === "none" || style.visibility === "hidden" || style.opacity === "0") {
         element.parentNode?.removeChild(element);
       }
 
@@ -127,13 +146,7 @@ const createPage = async (browser: puppeteer.Browser, w: number, h: number, url:
             return;
           }
 
-          let result;
-          try {
-            result = whats.getUniqueId(target);
-          } catch(e) {
-            target.id = "";
-            result = whats.getUniqueId(target);
-          }
+          const result = whats.getUniqueId(target);
 
           if (target.getAttribute("data-whats-element-wid") === null) {
             target.setAttribute("data-whats-element-wid", result.wid);
@@ -144,53 +157,62 @@ const createPage = async (browser: puppeteer.Browser, w: number, h: number, url:
             result.height = boundingBox.height;
 
             target.style.color = "red";
-            // target.style.backgroundColor = "red";
 
-            // @ts-ignore
-            window.__whats_element_result
+            // 过滤掉干扰元素， 比如 a > img, 那么 a 元素可以忽略
+            if (target.children.length === 0) {
               // @ts-ignore
-              ? window.__whats_element_result.push(result)
-              // @ts-ignore
-              : (window.__whats_element_result = [result]);
+              window.__whats_element_result
+                ? // @ts-ignore
+                  window.__whats_element_result.push(result)
+                : // @ts-ignore
+                  (window.__whats_element_result = [result]);
+            }
 
-            const display = getComputedStyle(target).display;
-            if (display === "block" || display === "table-cell") {
+            // const display = getComputedStyle(target).display;
+            // if (display === "block" || display === "table-cell") {
               const nodeList = target.childNodes;
               for (let k = 0; k < nodeList.length; k++) {
                 const node = nodeList[k];
-                
+
                 // 文字节点增加 span 标签
                 if (node.nodeType === 3 && node.textContent?.trim() !== "") {
-                  const text = node.textContent?.replace(/^[\r\n]|[\r\n]$/g, "");
+                  const text = node.textContent;
                   const textElement = document.createElement("span");
                   textElement.className = `span${k}`;
-                  textElement.innerText = text ?? "";
+                  textElement.textContent = text ?? "";
                   textElement.style.display = "inline";
                   textElement.style.float = "none";
                   textElement.style.padding = "0";
+                  textElement.style.margin = "0";
                   textElement.style.lineHeight = "inherit";
                   textElement.style.fontWeight = "inherit";
                   textElement.style.fontSize = "inherit";
 
-                  textElement.setAttribute("data-whats-element-wid", `${result.wid}>.${textElement.className}`);
-                  textElement.setAttribute("data-whats-element-type", result.type);
+                  textElement.setAttribute(
+                    "data-whats-element-wid",
+                    `${result.wid}>.${textElement.className}`
+                  );
+                  textElement.setAttribute(
+                    "data-whats-element-type",
+                    result.type
+                  );
 
                   node.replaceWith(textElement);
 
                   const boundingBox = textElement.getBoundingClientRect();
-                
+
                   // @ts-ignore
                   window.__whats_element_result.push({
-                      ...result,
-                      wid: `${result.wid}>.${textElement.className}`,
-                      width: boundingBox.width,
-                      height: boundingBox.height,
-                      left: boundingBox.left,
-                      top: boundingBox.top,
-                  })
+                    ...result,
+                    wid: `${result.wid}>.${textElement.className}`,
+                    width: boundingBox.width,
+                    height: boundingBox.height,
+                    left: boundingBox.left,
+                    top: boundingBox.top,
+                  });
                 }
-              }  
-            }
+              }
+            // }
           }
         }
       },
@@ -199,15 +221,16 @@ const createPage = async (browser: puppeteer.Browser, w: number, h: number, url:
   });
 
   for (let x = 0; x <= w; x += 15) {
-    for ( let y = start; y <= end; y += 10) {
+    for (let y = start; y <= end; y += 5) {
       await page.mouse.move(x, y);
     }
   }
 
-  const result = await page.evaluate(() => {
-    // @ts-ignore
-    return window.__whats_element_result;
-  }) || [];
+  const result =
+    (await page.evaluate(() => {
+      // @ts-ignore
+      return window.__whats_element_result;
+    })) || [];
 
   console.log(result.length);
   if (result.length === 0) {
