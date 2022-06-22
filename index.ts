517bf67fbb0b0965fd162e4bd38d6f95c111efd2
zhangyan <zhyabs@gmail.com>
2022-06-22 18:48:51 +0800

    feat: support hidden line

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ const main = async () => {
   const browser = await puppeteer.launch({
     headless: true,
     // slowMo: 1000,
-    args: ["--window-position=0,0"],
+    args: ["--window-position=0,0", "--disable-web-security"],
     devtools: true,
   });
 
@@ -67,9 +67,16 @@ const main = async () => {
         innerHeight: frameWin.innerHeight,
         left: boundingBox.left,
         top: boundingBox.top,
+        // @ts-ignore
+        attributes: [...frames[idx].attributes].reduce(
+          (s, n) => ({ ...s, [n.nodeName]: n.nodeValue }),
+          {}
+        ),
       };
     });
   });
+
+  console.info("get iframe, the count is", frameInfos.length);
 
   const gap = 1000;
   const count = Math.floor(html_height / gap);
@@ -81,6 +88,7 @@ const main = async () => {
     return page.waitForTimeout(10000 * idx).then(() => {
       return createPage(
         browser,
+        {},
         html_width,
         html_height,
         url,
@@ -96,6 +104,7 @@ const main = async () => {
     return page.waitForTimeout(10000).then(() => {
       return createPage(
         browser,
+        frameInfo.attributes,
         frameInfo.innerWidth,
         frameInfo.innerHeight,
         frameInfo.url,
@@ -111,11 +120,12 @@ const main = async () => {
 
   // const result = await createPage(
   //   browser,
+  //   {},
   //   html_width,
   //   html_height,
   //   url,
   //   4500,
-  //   4700
+  //   4600
   // );
   // const results = [result];
 
@@ -127,6 +137,7 @@ const main = async () => {
 
 const createPage = async (
   browser: puppeteer.Browser,
+  payload: { [key: string]: string },
   w: number,
   h: number,
   url: string,
@@ -149,6 +160,12 @@ const createPage = async (
 
   // 等待 whats-element 加载完成
   await page.waitForTimeout(3000);
+
+  // 兼容 iframe 属性
+  await page.evaluate((payload) => {
+    document.body.setAttribute("marginwidth", payload.marginwidth);
+    document.body.setAttribute("marginheight", payload.marginheight);
+  }, payload);
 
   // 删除display:none的元素，防止 hover 显示
   await page.evaluate(() => {
@@ -182,6 +199,15 @@ const createPage = async (
     // 滚动条会导致定位出现偏移
     document.body.style.overflow = "hidden";
 
+    function commit(payload: any) {
+      // @ts-ignore
+      window.__whats_element_result
+        ? // @ts-ignore
+          window.__whats_element_result.push(payload)
+        : // @ts-ignore
+          (window.__whats_element_result = [payload]);
+    }
+
     document.body.addEventListener(
       "mouseover",
       (event) => {
@@ -208,14 +234,33 @@ const createPage = async (
 
             target.style.color = "red";
 
+            // TODO
+            // 如果只有 textNode, 并且 textContent length 为 0， 忽略
+            if (target.childNodes.length === 1) {
+              const child = target.childNodes[0];
+              if (
+                child.nodeType === 3 &&
+                child?.textContent?.trim().length === 0
+              ) {
+                return;
+              }
+            }
+
             // 过滤掉干扰元素， 比如 a > img, 那么 a 元素可以忽略
-            if (target.children.length === 0) {
-              // @ts-ignore
-              window.__whats_element_result
-                ? // @ts-ignore
-                  window.__whats_element_result.push(result)
-                : // @ts-ignore
-                  (window.__whats_element_result = [result]);
+            // a > 只有 textNode, 那么 a 元素可以忽略
+            // 自闭标签，如 <img />, <br />, <input />, <textarea />, <select /> 需要计算在内
+            if (
+              target.children.length === 0 &&
+              (Array.from(target.childNodes).some((v) => v.nodeType !== 3) ||
+                ["img", "br", "input", "textarea", "canvas"].includes(
+                  target.tagName.toLowerCase()
+                ))
+            ) {
+              commit(result);
+            }
+
+            if (["select"].includes(target.tagName.toLowerCase())) {
+              commit(result);
             }
 
             const nodeList = target.childNodes;
@@ -245,19 +290,46 @@ const createPage = async (
                   result.type
                 );
 
-                node.replaceWith(textElement);
+                const range = document.createRange();
+                range.selectNodeContents(node);
+                const boundingBox = range.getBoundingClientRect();
+                const targetBoundingBox = { ...boundingBox };
+                // const boundingBox = textElement.getBoundingClientRect();
 
-                const boundingBox = textElement.getBoundingClientRect();
+                // 处理 横向 溢出换行文字溢出的情况
+                const parentElementBoundingBox =
+                  target.parentElement?.getBoundingClientRect() ?? {
+                    width: Number.MAX_VALUE,
+                    height: Number.MAX_VALUE,
+                  };
 
-                // @ts-ignore
-                window.__whats_element_result.push({
+                // const minw = Math.min(result.width, boundingBox.width, parentElementBoundingBox.width);
+                const minh = Math.min(
+                  result.height,
+                  boundingBox.height,
+                  parentElementBoundingBox.height
+                );
+
+                const clientRects = range.getClientRects();
+                const firstClientRect = clientRects[0];
+                if (
+                  firstClientRect &&
+                  minh < firstClientRect.height * 2
+                ) {
+                  // 仅取第一行文本
+                  targetBoundingBox.width = firstClientRect.width;
+                  targetBoundingBox.height = firstClientRect.height;
+                  targetBoundingBox.left = firstClientRect.left;
+                  targetBoundingBox.top = firstClientRect.top;
+                }
+
+                commit({
                   ...result,
-                  wid: `${result.wid}>.${textElement.className}`,
-                  width: boundingBox.width,
-                  height: boundingBox.height,
-                  left: boundingBox.left,
-                  top: boundingBox.top,
+                  wid: `span${k}`,
+                  ...targetBoundingBox,
                 });
+
+                node.replaceWith(textElement);
               }
             }
           }
@@ -267,7 +339,7 @@ const createPage = async (
     );
   });
 
-  for (let x = 0; x <= w; x += 15) {
+  for (let x = 0; x <= w; x += 10) {
     for (let y = start; y <= end; y += 5) {
       await page.mouse.move(x, y);
     }
@@ -289,6 +361,8 @@ const createPage = async (
     re.left = re.left + dx;
     re.top = re.top + dy;
   });
+
+  await page.close();
 
   return result ?? [];
 };
